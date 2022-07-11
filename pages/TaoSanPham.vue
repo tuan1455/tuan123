0f3f6ff2e3390c5a2f70231de727948b297b3b69
htatuan <tuan145514@gmail.com>
2022-07-11 17:32:50 +0900

    ggggggggggggg

---
+++ pages/TaoSanPham.vue
@@ -0,0 +1,20 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class>
+  <input v-model="checkedNames" type="text" placeholder="Search..">
+ </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      checkedNames: ""
+    }
+  }
+}
+</script>
+<style>
+</style>
+
